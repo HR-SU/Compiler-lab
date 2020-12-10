6a5565afb9d0372a85ca396651d9649494d5045b
HR-SU <1286804583@qq.com>
2020-12-10 22:05:01 +0800

    liveness analysis

--- flowgraph.c
+++ flowgraph.c
@@ -16,20 +16,68 @@
 
 Temp_tempList FG_def(G_node n) {
 	//your code here.
-	return NULL;
+	AS_instr instr = G_nodeInfo(n);
+	if(instr->kind == I_OPER)
+		return instr->u.OPER.dst;
+	else if(instr->kind == I_MOVE)
+		return instr->u.MOVE.dst;
+	else
+		return NULL;
 }
 
 Temp_tempList FG_use(G_node n) {
 	//your code here.
-	return NULL;
+	AS_instr instr = G_nodeInfo(n);
+	if(instr->kind == I_OPER)
+		return instr->u.OPER.src;
+	else if(instr->kind == I_MOVE)
+		return instr->u.MOVE.src;
+	else
+		return NULL;
 }
 
 bool FG_isMove(G_node n) {
 	//your code here.
-	return TRUE;
+	AS_instr instr = G_nodeInfo(n);
+	return instr->kind == I_MOVE;
 }
 
 G_graph FG_AssemFlowGraph(AS_instrList il, F_frame f) {
 	//your code here.
-	return NULL;
+	G_graph flowGraph = G_Graph();
+
+	bool isSeq = TRUE;
+	G_node oldNode = NULL, newNode = NULL;
+	AS_instr oldIns = NULL;
+	for(AS_instrList ilist = il; ilist; ilist = ilist->tail) {
+		newNode = G_Node(flowGraph, ilist->head);
+		if(oldNode) {
+			if(oldIns->kind == I_OPER && oldIns->u.OPER.jumps)
+				isSeq = FALSE;
+			else
+				isSeq = TRUE;
+			if(isSeq == TRUE) {
+				G_addEdge(oldNode, newNode);
+			}
+		}
+		oldIns = ilist->head;
+		oldNode = newNode;
+	}
+	
+	for(G_nodeList nl = G_nodes(flowGraph); nl; nl = nl->tail) {
+		AS_instr instr = G_nodeInfo(nl->head);
+		if(instr->kind == I_OPER && instr->u.OPER.jumps) {
+			for(Temp_labelList labels = instr->u.OPER.jumps->labels; labels;
+				labels = labels->tail) {
+				for(G_nodeList nodes = G_nodes(flowGraph); nodes; nodes = nodes->tail) {
+					AS_instr ins = G_nodeInfo(nodes->head);
+					if(ins->kind == I_LABEL && ins->u.LABEL.label == labels->head) {
+						G_addEdge(nl->head, nodes->head);
+						break;
+					}
+				}
+			}
+		}
+	}
+	return flowGraph;
 }
--- liveness.c
+++ liveness.c
@@ -19,15 +19,167 @@ Live_moveList Live_MoveList(G_node src, G_node dst, Live_moveList tail) {
 	return lm;
 }
 
+Temp_tempList tempUnion(Temp_tempList l1, Temp_tempList l2) {
+    Temp_tempList result = NULL, last = NULL;
+    Temp_temp next;
+    
+    while(l1 && l2) {
+        if(Temp_int(l1->head) < Temp_int(l2->head)) {
+            next = l1->head;
+            l1 = l1->tail;
+        }
+        else if(Temp_int(l1->head) > Temp_int(l2->head)) {
+            next = l2->head;
+            l2 = l2->tail;
+        }
+        else {
+            next = l1->head;
+            l1 = l1->tail; l2 = l2->tail;
+        }
+        if(result == NULL) last = result = Temp_TempList(next, NULL);
+        else last = last->tail = Temp_TempList(next, NULL);
+    }
+    while(l1) {
+        next = l1->head;
+        if(result == NULL) last = result = Temp_TempList(next, NULL);
+        else last = last->tail = Temp_TempList(next, NULL);
+        l1 = l1->tail;
+    }
+    while(l2) {
+        next = l2->head;
+        if(result == NULL) last = result = Temp_TempList(next, NULL);
+        else last = last->tail = Temp_TempList(next, NULL);
+        l2 = l2->tail;
+    }
+    return result;
+}
+
+Temp_tempList tempDiff(Temp_tempList l1, Temp_tempList l2) {
+    Temp_tempList result = NULL, last = NULL;
+    while(l1 && l2) {
+        if(Temp_int(l1->head) < Temp_int(l2->head)) {
+            if(result == NULL) last = result = Temp_TempList(l1->head, NULL);
+            else last = last->tail = Temp_TempList(l1->head, NULL);
+            l1 = l1->tail;
+        }
+        else if(Temp_int(l1->head) > Temp_int(l2->head)) {
+            l2 = l2->tail;
+        }
+        else {
+            l1 = l1->tail;
+            l2 = l2->tail;
+        }
+    }
+    while(l1) {
+        if(result == NULL) last = result = Temp_TempList(l1->head, NULL);
+        else last = last->tail = Temp_TempList(l1->head, NULL);
+        l1 = l1->tail;
+    }
+    return result;
+}
+
+Temp_tempList tempReorder(Temp_tempList list) {
+	if(list == NULL) return NULL;
+	Temp_tempList result = NULL;
+	for(; list; list = list->tail) {
+		if(result == NULL)
+			result = Temp_TempList(list->head, NULL);
+		else {
+			if(Temp_int(result->head) > Temp_int(list->head)) {
+				result = Temp_TempList(list->head, result);
+				continue;
+			}
+			Temp_tempList tmp = result;
+			while(tmp->tail && Temp_int(tmp->tail->head) < Temp_int(list->head))
+				tmp = tmp->tail;
+			if(Temp_int(tmp->head) == Temp_int(list->head)) continue;
+			tmp->tail = Temp_TempList(list->head, tmp->tail);
+		}
+	}
+	return result;
+}
+
+void tempFree(Temp_tempList list) {
+    while(list) {
+        Temp_tempList l = list;
+        list = list->tail;
+        free(l);
+    }
+}
+
+G_node findNodeByTemp(G_graph g, Temp_temp temp) {
+	for(G_nodeList nodes = G_nodes(g); nodes; nodes = nodes->tail)
+		if(G_nodeInfo(nodes->head) == temp) return nodes->head;
+	return G_Node(g, temp);
+}
 
 Temp_temp Live_gtemp(G_node n) {
 	//your code here.
-	return NULL;
+	return G_nodeInfo(n);
 }
 
 struct Live_graph Live_liveness(G_graph flow) {
 	//your code here.
 	struct Live_graph lg;
+	G_table inTable = G_empty(), outTable = G_empty(), defTable = G_empty();
+	for(G_nodeList nodes = G_nodes(flow); nodes; nodes = nodes->tail) {
+		Temp_tempList use = FG_use(nodes->head);
+		G_enter(inTable, nodes->head, tempReorder(use));
+		Temp_tempList def = FG_def(nodes->head);
+		G_enter(defTable, nodes->head, tempReorder(def));
+		G_enter(outTable, nodes->head, NULL);
+	}
+	bool flag = TRUE;
+	while(flag) {
+		flag = FALSE;
+		for(G_nodeList nodes = G_nodes(flow); nodes; nodes = nodes->tail) {
+			Temp_tempList in = G_look(inTable, nodes->head);
+			Temp_tempList out = G_look(outTable, nodes->head);
+			Temp_tempList def = G_look(defTable, nodes->head);
+			Temp_tempList diff = tempDiff(out, def);
+			Temp_tempList newIn = tempUnion(in, diff);
+			G_nodeList succs = G_succ(nodes->head);
+			Temp_tempList newOut = NULL;
+			for(; succs; succs = succs->tail) {
+				Temp_tempList tmp = newOut;
+				newOut = tempUnion(tmp, G_look(inTable, succs->head));
+				tempFree(tmp);
+			}
+			Temp_tempList l1=tempDiff(newOut, out), l2=tempDiff(newIn, in);
+			if(l1 || l2) {
+				flag = TRUE;
+				tempFree(l1); tempFree(l2);
+			}
+			G_enter(inTable, nodes->head, newIn);
+			G_enter(outTable, nodes->head, newOut);
+			tempFree(diff); tempFree(in); tempFree(out);
+		}
+	}
+
+	G_graph conflict = G_Graph();
+	Live_moveList moveList = NULL;
+	for(G_nodeList nodes = G_nodes(flow); nodes; nodes = nodes->tail) {
+		Temp_tempList def = FG_def(nodes->head);
+		Temp_tempList out = G_look(outTable, nodes->head);
+		for(; def; def = def->tail) {
+			G_node d = findNodeByTemp(conflict, def);
+			if(FG_isMove(nodes->head)) {
+				G_node s = findNodeByTemp(conflict, FG_use(nodes->head)->head);
+				moveList = Live_MoveList(s, d, moveList);
+			}
+			for(; out; out = out->tail) {
+				G_node o = findNodeByTemp(conflict, out);
+				if(FG_isMove(nodes->head)) {
+					if(FG_use(nodes->head)->head == out) continue;
+				}
+				if(G_goesTo(d, o) || G_goesTo(o, d)) continue;
+				else G_addEdge(d, o);
+			}
+		}
+	}
+
+	lg.graph = conflict;
+	lg.moves = moveList;
 	return lg;
 }
 
