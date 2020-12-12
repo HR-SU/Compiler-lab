713618075a1b1e4cccf74d7a24f6cdc751168ae7
HR-SU <1286804583@qq.com>
2020-12-13 00:09:27 +0800

    fix some bugs

--- regalloc.c
+++ regalloc.c
@@ -13,11 +13,19 @@
 #include "table.h"
 #include "flowgraph.h"
 
+/* void showNode(void *info) {
+	Temp_map F_tempMap = F_TempMap();
+	Temp_map map = Temp_layerMap(F_tempMap, Temp_name());
+	Temp_temp temp = (Temp_temp)info;
+	printf("%s:\n", Temp_look(map, temp));
+} */
+
 struct RA_result RA_regAlloc(F_frame f, AS_instrList il) {
 	//your code here
 	G_graph fg = FG_AssemFlowGraph(il, f);
 	struct Live_graph lg = Live_liveness(fg);
 	F_tempMap = F_TempMap();
+	// G_show(stdout, G_nodes(lg.graph), showNode);
 	struct COL_result result = COL_color(lg.graph, F_tempMap, F_registers(),
 		lg.moves, lg.tempToMove);
 	struct RA_result ret;
